751b09e6233b46a6ba5ee18399b1c070f7550a3f
Shamil <shamilyassin799@gmail.com>
2024-03-09 01:55:42 +0700

    Menambah

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
   <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
   <h1>Hello, this is laravel CSS home</h1>
+  <link rel=”stylesheet” href=”https://cdn.tailwindcss.com/3.4.1”>
   <img src="{{ asset('gambar/david de gea.JPEG') }}" alt="Laravel" width="300" h>
   <img src="{{ asset('gambar/david de gea1.JPEG') }}" alt="Laravel" width="300" h>
 </div>
