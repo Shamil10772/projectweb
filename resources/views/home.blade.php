3a5628725ccb660fee67f6d8d4cd6ff6a6a84b60
Shamil <shamilyassin799@gmail.com>
2024-03-09 19:25:26 +0700

    Seperti biasa merubah file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,24 @@
-<div>
-    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Home</title>
+<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.tailwindcss.com/3.4.1">
+<link rel="stylesheet" href="/style/tailwindcss3.4.1.js">
 </head>
-  <h1>Hello, this is laravel CSS home</h1>
-  <link rel=”stylesheet” href=”https://cdn.tailwindcss.com/3.4.1”>
-  <img src="{{ asset('gambar/david de gea.JPEG') }}" alt="Laravel" width="300" h>
-  <img src="{{ asset('gambar/david de gea1.JPEG') }}" alt="Laravel" width="300" h>
+<body class="bg-gray-200">
+
+<header class="bg-gray-900 text-white py-4 text-center">
+    <h1>Hello, this is laravel CSS home</h1>
+</header>
+
+<div class="container mx-auto mt-8 flex justify-center">
+    <img src="{{ asset('gambar/david de gea.JPEG') }}" alt="Laravel" width="300" class="mr-4">
+    <img src="{{ asset('gambar/david de gea1.JPEG') }}" alt="Laravel" width="300">
 </div>
+
+</body>
+</html>
 
