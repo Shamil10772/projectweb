0962aa5b2975836871f8f469fa102b54edc71616
Shamil <shamilyassin799@gmail.com>
2024-03-16 23:20:50 +0700

    Menambahkan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,20 +11,17 @@
     <div class="logo">
       <img src="logo.png" alt="Logo Website">
     </div>
-    <h1>Nama Website Anda</h1>
     <nav>
       <ul>
         <li><a href="#">Beranda</a></li>
         <li><a href="#">Tentang Kami</a></li>
-        <li><a href="#">Produk</a></li>
+        <a href="{{ route('dashboard') }}">Dashboard</a>
         <li><a href="#">Layanan</a></li>
         <li><a href="#">Kontak</a></li>
         <li><a href="#">Menu Lainnya</a></li>
       </ul>
     </nav>
   </header>
-  <main>
-    </main>
   <footer>
   </footer>
 </body>
