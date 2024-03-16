a38bb95ca1023c5dc62e03806a35b709991a6e95
Shamil <shamilyassin799@gmail.com>
2024-03-17 00:31:24 +0700

    Memperbarui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Go to Next Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
 </head>
 <body>
-  <h1>ini adalah halaman dashboard</h1>
-  <p>Dashboard.</p>
-
-  <!-- Tautan untuk pergi ke halaman selanjutnya -->
-  <a href="http://localhost:8000/listbarang">Go to List barang</a>
-  <link rel="stylesheet" href="/style/tailwindcss3.4.1.js">
+    <h1>Harus login terlebih dahulu</h1>
 </body>
 </html>
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,21 @@
 <!DOCTYPE html>
-<html lang="id">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Nama Website Anda</title>
-  <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar Learning</title>
 </head>
 <body>
-  <header>
-    <div class="logo">
-      <img src="logo.png" alt="Logo Website">
-    </div>
-    <nav>
+  <div class="fContainer">
+    <nav class="wrapper">
       <ul>
-        <li><a href="#">Beranda</a></li>
-        <li><a href="#">Tentang Kami</a></li>
-        <a href="{{ route('dashboard') }}">Dashboard</a>
-        <li><a href="#">Layanan</a></li>
-        <li><a href="#">Kontak</a></li>
-        <li><a href="#">Menu Lainnya</a></li>
+        <li><a></a></li>
+        <li><a></a></li>
+        <li><a></a></li>
       </ul>
     </nav>
-  </header>
-  <footer>
-  </footer>
+  </div>
 </body>
 </html>
+
